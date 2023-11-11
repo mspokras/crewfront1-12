d95d9e6bf685e202c84f0b5d8c1ba570c7ebd9e9
Mykhailo Pokras <mspokras@gmail.com>
2023-11-11 22:43:16 +0200

    Mocked data for orgs panel

--- src/entities/Organisation/OrgCard/OrgCard.tsx
+++ src/entities/Organisation/OrgCard/OrgCard.tsx
@@ -3,28 +3,38 @@ import SectionButton from '../../../shared/components/Button/SectionButton/Secti
 import star from '../../../assets/icons/star.svg'
 import './OrgCard.scss';
 
-const OrgCard = () => {
+interface PropTypes {
+  rating: number;
+  name: string;
+  orders: number;
+  funds: number;
+  phone: string;
+  email: string;
+}
+
+const OrgCard = (props: PropTypes) => {
+  const { rating, name, orders, funds, phone, email } = props;
   return (
     <div className='org-card'>
       <div className='org-content'>
         <div className='org-rating'>
           <img src={star} alt="star" />
-          <div className='org-avnum'>4.5</div>
+          <div className='org-avnum'>{rating}</div>
         </div>
-        <div className='org-name'>McDonald's</div>
+        <div className='org-name'>{name}</div>
         <div className='org-stats'>
           <div className='org-orders'>
-            <div className='org-orders-quantity'>388</div>
+            <div className='org-orders-quantity'>{orders}</div>
             <div className='org-orders-label'>Orders Made</div>
           </div>
           <div className='org-funds'>
-            <div className='org-funds-quantity'>$90.000</div>
+            <div className='org-funds-quantity'>${funds}</div>
             <div className='org-funds-label'>Available Funds</div>
           </div>
         </div>
         <div className='org-contact'>
-          <div className='org-whatsapp'>+1(555)222-33-22</div>
-          <div className='org-email'>org.work.email@gmail.com</div>
+          <div className='org-whatsapp'>{phone}</div>
+          <div className='org-email'>{email}</div>
         </div>
       </div>
       <SectionButton label="Show organisation" />
