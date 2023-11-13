06c6cb3a7f49bf897a523ad19f1a7bac57e16525
Mykhailo Pokras <mspokras@gmail.com>
2023-11-13 16:28:37 +0200

    Order page finished

--- src/entities/Order/OrderCard/OrderCard.tsx
+++ src/entities/Order/OrderCard/OrderCard.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './OrderCard.scss';
+import SectionButton from '../../../shared/components/Button/SectionButton/SectionButton';
 
-const OrderCard = () => {
+interface PropTypes {
+  number: number;
+  date: string;
+  funds: number
+}
+
+const OrderCard = (props: PropTypes) => {
+  const { number, date, funds } = props;
+
   return (
     <div className='order-card'>
-      order card
+      <div className="order-content">
+        <div className="order-number">#{number}</div>
+        <div className="order-date">{date}</div>
+        <div className="order-funds">${funds}</div>
+      </div>
+      <SectionButton label="More details" isFilled={true} />
     </div>
   );
 };
--- src/widgets/OrderCards/OrderCards.tsx
+++ src/widgets/OrderCards/OrderCards.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './OrderCards.scss';
 import OrderCard from '../../entities/Order/OrderCard/OrderCard';
+import cardsData from './ordersData.json';
 
 const OrderCards = () => {
   return (
     <div className='order-cards'>
-      <OrderCard />
-      <OrderCard />
-      <OrderCard />
+      {cardsData.map((card, index) => (
+        <OrderCard key={index} {...card} />
+      ))}
     </div>
   );
 };
